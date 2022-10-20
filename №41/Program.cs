0779ca84e00b3b41d02745a3af5137514f1b1465
Ryaba1488 <ilya-ryabov03@yandex.ru>
2022-10-20 14:09:29 +0300

    homework

---
+++ №41/Program.cs
@@ -0,0 +1,18 @@
+﻿int UserSum(int size)
+{
+   int number;
+   int Result = 0; 
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Введите число ");
+        int.TryParse(Console.ReadLine(), out number);
+        if(number > 0)
+        {
+            Result = Result + number; 
+        }
+    }
+    return Result;
+}
+
+int FinishResult = UserSum(6);
+Console.WriteLine($"Cумма чисел больше нуля равна {FinishResult}");
